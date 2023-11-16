6eebf9da5d81a7e342da1161f59e3fe2f53f2408
Amina <113165708+asmina18@users.noreply.github.com>
2023-11-16 15:30:39 +0100

    manifest og sw vikker

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -6,13 +6,20 @@ function openSettingsModal() {
 }
 
 // Функция для закрытия модального окна
-function closeSettingsModal() {
-   var modal = document.getElementById("settings-modal");
-   modal.style.display = "none";
-}
+// function closeSettingsModal() {
+//    var modal = document.getElementById("settings-modal");
+//    modal.style.display = "none";
+// }
 
 // Обработчик события для кнопки открытия настроек
-document.getElementById("open-settings").addEventListener("click", openSettingsModal);
+// document.getElementById("open-settings").addEventListener("click", openSettingsModal);
 
 // Обработчик события для кнопки закрытия настроек
-document.getElementById("close-settings").addEventListener("click", closeSettingsModal);
+// document.getElementById("close-settings").addEventListener("click", closeSettingsModal);
+
+//Registrerer serviceworker og sender beskeder i consolen
+if ('serviceWorker' in navigator) {
+   navigator.serviceWorker.register('../../sw.js')
+       .then(reg => console.log('service worker registered', reg))
+       .catch(err => console.error('service worker not registered', err))
+}
